7d30834c895dc9d279fa442246737ce7d53cf076
vitorpepe <34254106+vitorpepe@users.noreply.github.com>
2023-04-20 18:02:45 -0300

    Update SemMutex.c

--- SemMutex.c
+++ SemMutex.c
@@ -1,3 +1,11 @@
+/*
+ Sistemas Operacionais
+ Prof. Eduardo Ferreira
+Calculo de Pi - Sem Mutex
+ Nome: Vitor Pepe
+ TIA: 31852963
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -29,7 +37,7 @@ void* Thread_sum(void* rank) {
 
 int main(int argc, char* argv[]) {
   long thread;
-  pthread_t* thread_handles;
+  pthread_t* thread_handles; // vetor para armazenar os identificadores das threads
 
   printf("Digite o numero de threads utilizadas no cálculo:\n");
   scanf("%ld", &thread_count);
@@ -37,25 +45,36 @@ int main(int argc, char* argv[]) {
   n = 1000000; /* Valor padrão de N */
   printf("Calculando Pi utilizando %lld iteraçoes\n", n);
 
-  thread_handles = malloc(thread_count*sizeof(pthread_t));
-  sum = malloc(thread_count*sizeof(double));
+  thread_handles = malloc(thread_count*sizeof(pthread_t)); // aloca memória para o vetor de identificadores das threads
+  sum = malloc(thread_count*sizeof(double)); // aloca memória para o vetor que armazenará a soma de cada thread
 
+  // inicializa o vetor de soma com 0
   for (thread = 0; thread < thread_count; thread++)
     sum[thread] = 0.0;
 
+  // cria as threads e as executa
   for (thread = 0; thread < thread_count; thread++)
     pthread_create(&thread_handles[thread], NULL, Thread_sum, (void*) thread);
 
+  // espera todas as threads terminarem de executar
   for (thread = 0; thread < thread_count; thread++)
     pthread_join(thread_handles[thread], NULL);
 
+  // soma as somas parciais de cada thread para obter o valor final de Pi
   double pi = 0.0;
   for (thread = 0; thread < thread_count; thread++)
     pi += sum[thread];
 
-  pi = 4.0*pi;
+  pi = 4.0*pi; // multiplica por 4 para obter o valor final de Pi
 
   printf("pi = %.15f\n", pi);
+
+  // libera a memória alocada
+  free(thread_handles);
+  free(sum);
+
+  return 0;
+}
 
   free(thread_handles);
   free(sum);
